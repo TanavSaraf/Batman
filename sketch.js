c02e0d23155ba509fde7f761fb628e262a95e9c9
TanavSaraf <75167346+TanavSaraf@users.noreply.github.com>
2021-05-08 16:42:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,8 +8,15 @@ var boyImg;
 var boy,umbrella;
 var dropsA=[];
 var ground;
+var bolt;
+var a,b,c,d;
+var ani;
 function preload(){
     boyImg=loadImage("proC41Img.png");
+    a=loadImage("1.png");
+    b=loadImage("2.png");
+    c=loadImage("3.png");
+    d=loadImage("4.png");
 }
 
 function setup(){
@@ -20,6 +27,14 @@ function setup(){
     umbrella = Bodies.circle(boy.x,boy.y,100,{isStatic:true});
     World.add(world,umbrella);
     ground=createSprite(200,560,450,20);
+    bolt=createSprite(200,10);
+    bolt.addImage("a",a);
+    bolt.addImage("b",b);
+    bolt.addImage("c",c);
+    bolt.addImage("d",d);
+    bolt.scale=0.5
+
+    bolt.visible=false;
     for(var i=0;i<=60;i++)
     {
         
@@ -48,6 +63,29 @@ function draw(){
     boy.scale=2;
     boy.addImage("AAB",boyImg);
     
+    if(frameCount%70===0)
+    {
+        bolt.visible=true
+        ani=random(1,4);
+        if(ani===1)
+        {
+            bolt.changeImage("a",a)
+        }else if(ani===2)
+        {
+            bolt.changeImage("b",b)
+
+        }else if(ani===3)
+        {
+            bolt.changeImage("c",c)
+        }else if(ani===4)
+        {
+            bolt.changeImage("d",d)
+            
+        }
+
+    }else{
+        bolt.visible=false
+    }
 
     drawSprites()
 }   
